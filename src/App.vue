8e4855cba7d5f427514dec84207ae889ea190fc1
Igor Zinken <igor.zinken@igorski.nl>
2020-12-10 20:57:22 +0100

    Initial project outline added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Nicéphore</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    components: {
+
+    }
+};
 </script>
 
 <style lang="scss">
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+// igorski.nl maintains its own service worker registration, if your
+// custom app requires PWA support, uncomment the following line
+//import './registerServiceWorker'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    store,
+    render: h => h( App )
+}).$mount( '#app' );
