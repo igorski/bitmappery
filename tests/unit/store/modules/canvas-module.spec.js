98603d6c204126dd86264995cb029cb39828963f
Igor Zinken <igor.zinken@igorski.nl>
2020-12-22 11:07:07 +0100

    Remove rudimentary tests

--- tests/unit/store/modules/canvas-module.spec.js
+++ tests/unit/store/modules/canvas-module.spec.js
@@ -4,11 +4,6 @@ const { getters, mutations } = storeModule;
 
 describe( "Vuex canvas module", () => {
     describe( "getters", () => {
-        it( "should be able to retrieve the current zCanvas instance", () => {
-            const state = { zCanvas: { foo: "bar" } };
-            expect( getters.zCanvas( state )).toEqual( state.zCanvas );
-        });
-
         it( "should be able to retrieve the zCanvas instance base dimensions", () => {
             const state = { zCanvasBaseDimensions: { width: 10, height: 5 } };
             expect( getters.zCanvasBaseDimensions( state )).toEqual( state.zCanvasBaseDimensions );
@@ -16,13 +11,6 @@ describe( "Vuex canvas module", () => {
     });
 
     describe( "mutations", () => {
-        it( "should be able to set the current zCanvas instance", () => {
-            const state  = { zCanvas: null };
-            const canvas = { foo: "bar" };
-            mutations.setZCanvas( state, canvas );
-            expect( state.zCanvas ).toEqual( canvas );
-        });
-
         it( "should be able to set the zCanvas instance base dimensions", () => {
             const state = { zCanvasBaseDimensions: { width: 10, height: 5 } };
             mutations.setZCanvasBaseDimensions( state, { width: 50, height: 40 } );
