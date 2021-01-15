a0a5cb465e39d5eae5a100f834bb80cc949b3233
Igor Zinken <igor.zinken@igorski.nl>
2021-01-15 09:29:43 +0100

    Caught null pointer exception on outline rendering

--- src/components/ui/zcanvas/interaction-pane.js
+++ src/components/ui/zcanvas/interaction-pane.js
@@ -287,7 +287,7 @@ function drawSelectionOutline( ctx, zCanvas, viewport, selection, color, current
         );
     });
     // for lasso selections, draw line to current cursor position
-    if ( currentPosition?.x !== 0 && currentPosition?.y !== 0 ) {
+    if ( currentPosition && currentPosition.x !== 0 && currentPosition.y !== 0 ) {
         ctx.lineTo(( .5 + currentPosition.x ) << 0, ( .5 + currentPosition.y ) << 0 );
     }
     ctx.stroke();
