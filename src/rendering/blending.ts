f00e2d12586fc57e9f4e3f95dcf935491cad4242
Igor Zinken <igor.zinken@igorski.nl>
2023-04-10 08:50:19 +0200

    cleaner code

--- src/rendering/blending.ts
+++ src/rendering/blending.ts
@@ -32,6 +32,8 @@ const { cvs, ctx } = createCanvas();
 
 /**
  * Retrieve a pooled empty context to use for drawing layer content on prior to blending
+ * onto underlying Document context
+ *
  * @param {HTMLCanvasElement} documentCanvas to match dimensions of
  */
 export const getBlendContext = ( documentCanvas: HTMLCanvasElement ): CanvasRenderingContext2D => {
@@ -41,7 +43,7 @@ export const getBlendContext = ( documentCanvas: HTMLCanvasElement ): CanvasRend
     cvs.height = height;
 
     return ctx;
-}
+};
 
 /**
  * Blend a Layer onto the underlying Document content
@@ -49,7 +51,7 @@ export const getBlendContext = ( documentCanvas: HTMLCanvasElement ): CanvasRend
  * @param {CanvasRenderingContext2D} dest the context that contains all rendered content below the Layer
  * @param {CanvasRenderingContext2D} layer the context that contains the content of the Layer (see getBlendContext)
  * @param {BlendModes} blendMode the blend mode to use
- * @param {Rectangle?} bounds optional bounds to constraint blending within defaults to full dest size
+ * @param {Rectangle?} bounds optional bounds to constrain blending within. defaults to full dest size
  */
 export const blendLayer = ( dest: CanvasRenderingContext2D, layer: CanvasRenderingContext2D,
                             blendMode: BlendModes, bounds?: Rectangle ): void => {
@@ -61,11 +63,11 @@ export const blendLayer = ( dest: CanvasRenderingContext2D, layer: CanvasRenderi
     }
 
     const srcImageData: ImageData = layer.getImageData( left, top, width, height );
-	const dstImageData: ImageData = dest.getImageData( left, top, width, height );
+    const dstImageData: ImageData = dest.getImageData( left, top, width, height );
 
-	const src: Uint8ClampedArray = srcImageData.data;
-	const dst: Uint8ClampedArray = dstImageData.data;
-	const size = dst.length;
+    const src: Uint8ClampedArray = srcImageData.data;
+    const dst: Uint8ClampedArray = dstImageData.data;
+    const size = dst.length;
 
     let rgb: RGB;
     let hsl: RGB;
@@ -75,161 +77,161 @@ export const blendLayer = ( dest: CanvasRenderingContext2D, layer: CanvasRenderi
 
     for ( let px = 0; px < size; px += 4 ) {
         // alpha channel
-		const sA  = src[ px + 3 ] / 255;
-		const dA  = dst[ px + 3 ] / 255;
-		const dA2 = ( sA + dA - sA * dA );
+        const sA  = src[ px + 3 ] / 255;
+        const dA  = dst[ px + 3 ] / 255;
+        const dA2 = ( sA + dA - sA * dA );
 
-		dst[ px + 3 ] = dA2 * 255;
+        dst[ px + 3 ] = dA2 * 255;
 
         // grab RGB values
-		const r1 = dst[ px ], g1 = dst[ px + 1 ], b1 = dst[ px + 2 ];
-		const r2 = src[ px ], g2 = src[ px + 1 ], b2 = src[ px + 2 ];
+        const r1 = dst[ px ], g1 = dst[ px + 1 ], b1 = dst[ px + 2 ];
+        const r2 = src[ px ], g2 = src[ px + 1 ], b2 = src[ px + 2 ];
 
-		const sRA = r2 / 255 * sA;
-		const dRA = r1 / 255 * dA;
-		const sGA = g2 / 255 * sA;
-		const dGA = g1 / 255 * dA;
-		const sBA = b2 / 255 * sA;
-		const dBA = b1 / 255 * dA;
+        const sRA = r2 / 255 * sA;
+        const dRA = r1 / 255 * dA;
+        const sGA = g2 / 255 * sA;
+        const dGA = g1 / 255 * dA;
+        const sBA = b2 / 255 * sA;
+        const dBA = b1 / 255 * dA;
 
-		const demultiply = 255 / dA2;
+        const demultiply = 255 / dA2;
 
-		const f1 = dA * sA, f2 = dA - f1, f3 = sA - f1;
+        const f1 = dA * sA, f2 = dA - f1, f3 = sA - f1;
 
-		switch( blendMode ) {
+        switch( blendMode ) {
             default:
-			case BlendModes.SCREEN:
-				dst[ px ]     = ( sRA + dRA - sRA * dRA ) * demultiply;
-				dst[ px + 1 ] = ( sGA + dGA - sGA * dGA ) * demultiply;
-				dst[ px + 2 ] = ( sBA + dBA - sBA * dBA ) * demultiply;
+            case BlendModes.SCREEN:
+            	dst[ px ]     = ( sRA + dRA - sRA * dRA ) * demultiply;
+            	dst[ px + 1 ] = ( sGA + dGA - sGA * dGA ) * demultiply;
+            	dst[ px + 2 ] = ( sBA + dBA - sBA * dBA ) * demultiply;
                 break;
 
-			case BlendModes.MULTIPLY:
-				dst[ px ]     = ( sRA * dRA + sRA * ( 1 - dA ) + dRA * ( 1 - sA )) * demultiply;
-				dst[ px + 1 ] = ( sGA * dGA + sGA * ( 1 - dA ) + dGA * ( 1 - sA )) * demultiply;
-				dst[ px + 2 ] = ( sBA * dBA + sBA * ( 1 - dA ) + dBA * ( 1 - sA )) * demultiply;
+            case BlendModes.MULTIPLY:
+            	dst[ px ]     = ( sRA * dRA + sRA * ( 1 - dA ) + dRA * ( 1 - sA )) * demultiply;
+            	dst[ px + 1 ] = ( sGA * dGA + sGA * ( 1 - dA ) + dGA * ( 1 - sA )) * demultiply;
+            	dst[ px + 2 ] = ( sBA * dBA + sBA * ( 1 - dA ) + dBA * ( 1 - sA )) * demultiply;
                 break;
 
-			case BlendModes.DIFFERENCE:
-				dst[ px ]     = ( sRA + dRA - 2 * Math.min( sRA * dA, dRA * sA )) * demultiply;
-				dst[ px + 1 ] = ( sGA + dGA - 2 * Math.min( sGA * dA, dGA * sA )) * demultiply;
-				dst[ px + 2 ] = ( sBA + dBA - 2 * Math.min( sBA * dA, dBA * sA )) * demultiply;
+            case BlendModes.DIFFERENCE:
+            	dst[ px ]     = ( sRA + dRA - 2 * Math.min( sRA * dA, dRA * sA )) * demultiply;
+            	dst[ px + 1 ] = ( sGA + dGA - 2 * Math.min( sGA * dA, dGA * sA )) * demultiply;
+            	dst[ px + 2 ] = ( sBA + dBA - 2 * Math.min( sBA * dA, dBA * sA )) * demultiply;
                 break;
 
-			case BlendModes.LINEAR_DODGE:
-				dst[ px ]     = Math.min( sRA + dRA, 1 ) * demultiply;
-				dst[ px + 1 ] = Math.min( sGA + dGA, 1 ) * demultiply;
-				dst[ px + 2 ] = Math.min( sBA + dBA, 1 ) * demultiply;
+            case BlendModes.LINEAR_DODGE:
+            	dst[ px ]     = Math.min( sRA + dRA, 1 ) * demultiply;
+            	dst[ px + 1 ] = Math.min( sGA + dGA, 1 ) * demultiply;
+            	dst[ px + 2 ] = Math.min( sBA + dBA, 1 ) * demultiply;
                 break;
 
-			case BlendModes.OVERLAY:
-				dst[ px]      = f1 * blendOverlay( r1, r2 ) + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * blendOverlay( g1, g2 ) + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * blendOverlay( b1, b2 ) + f2 * b1 + f3 * b2;
-			    break;
+            case BlendModes.OVERLAY:
+            	dst[ px]      = f1 * blendOverlay( r1, r2 ) + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * blendOverlay( g1, g2 ) + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * blendOverlay( b1, b2 ) + f2 * b1 + f3 * b2;
+                break;
 
-			case BlendModes.HARD_LIGHT:
-				dst[ px]      = f1 * blendOverlay( r2, r1 ) + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * blendOverlay( g2, g1 ) + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * blendOverlay( b2, b1 ) + f2 * b1 + f3 * b2;
-			    break;
+            case BlendModes.HARD_LIGHT:
+            	dst[ px]      = f1 * blendOverlay( r2, r1 ) + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * blendOverlay( g2, g1 ) + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * blendOverlay( b2, b1 ) + f2 * b1 + f3 * b2;
+                break;
 
-			case BlendModes.COLOR_DODGE:
-				dst[ px]      = f1 * blendDodge( r1, r2 ) + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * blendDodge( g1, g2 ) + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * blendDodge( b1, b2 ) + f2 * b1 + f3 * b2;
-			    break;
+            case BlendModes.COLOR_DODGE:
+            	dst[ px]      = f1 * blendDodge( r1, r2 ) + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * blendDodge( g1, g2 ) + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * blendDodge( b1, b2 ) + f2 * b1 + f3 * b2;
+                break;
 
-			case BlendModes.COLOR_BURN:
-				dst[ px ]     = f1 * blendBurn( r1, r2 ) + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * blendBurn( g1, g2 ) + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * blendBurn( b1, b2 ) + f2 * b1 + f3 * b2;
-			    break;
+            case BlendModes.COLOR_BURN:
+            	dst[ px ]     = f1 * blendBurn( r1, r2 ) + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * blendBurn( g1, g2 ) + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * blendBurn( b1, b2 ) + f2 * b1 + f3 * b2;
+                break;
 
-			case BlendModes.DARKEN:
-				dst[ px ]     = f1 * ( r1 < r2 ? r1 : r2 ) + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * ( g1 < g2 ? g1 : g2 ) + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * ( b1 < b2 ? b1 : b2 ) + f2 * b1 + f3 * b2;
-			    break;
+            case BlendModes.DARKEN:
+            	dst[ px ]     = f1 * ( r1 < r2 ? r1 : r2 ) + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * ( g1 < g2 ? g1 : g2 ) + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * ( b1 < b2 ? b1 : b2 ) + f2 * b1 + f3 * b2;
+                break;
 
-			case BlendModes.LIGHTEN:
-				dst[ px ]     = ( sRA < dRA ? dRA : sRA ) * demultiply;
-				dst[ px + 1 ] = ( sGA < dGA ? dGA : sGA ) * demultiply;
-				dst[ px + 2 ] = ( sBA < dBA ? dBA : sBA ) * demultiply;
-			    break;
+            case BlendModes.LIGHTEN:
+            	dst[ px ]     = ( sRA < dRA ? dRA : sRA ) * demultiply;
+            	dst[ px + 1 ] = ( sGA < dGA ? dGA : sGA ) * demultiply;
+            	dst[ px + 2 ] = ( sBA < dBA ? dBA : sBA ) * demultiply;
+                break;
 
-			case BlendModes.EXCLUSION:
-				dst[ px ]     = ( dRA + sRA - 2 * dRA * sRA ) * demultiply;
-				dst[ px + 1 ] = ( dGA + sGA - 2 * dGA * sGA ) * demultiply;
-				dst[ px + 2 ] = ( dBA + sBA - 2 * dBA * sBA ) * demultiply;
-			    break;
+            case BlendModes.EXCLUSION:
+            	dst[ px ]     = ( dRA + sRA - 2 * dRA * sRA ) * demultiply;
+            	dst[ px + 1 ] = ( dGA + sGA - 2 * dGA * sGA ) * demultiply;
+            	dst[ px + 2 ] = ( dBA + sBA - 2 * dBA * sBA ) * demultiply;
+                break;
 
-			case BlendModes.SOFT_LIGHT:
-				dst[ px ]     = f1 * blendSoftlight( r1, r2 ) + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * blendSoftlight( g1, g2 ) + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * blendSoftlight( b1, b2 ) + f2 * b1 + f3 * b2;
-			    break;
+            case BlendModes.SOFT_LIGHT:
+            	dst[ px ]     = f1 * blendSoftlight( r1, r2 ) + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * blendSoftlight( g1, g2 ) + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * blendSoftlight( b1, b2 ) + f2 * b1 + f3 * b2;
+                break;
 
-			case BlendModes.LUMINOSITY:
-				hsl  = rgb2YCbCr( r1, g1, b1 );
-				hsl2 = rgb2YCbCr( r2, g2, b2 );
+            case BlendModes.LUMINOSITY:
+            	hsl  = rgb2YCbCr( r1, g1, b1 );
+            	hsl2 = rgb2YCbCr( r2, g2, b2 );
                 rgb  = YCbCr2rgb( hsl2.r, hsl.g, hsl.b );
 
                 dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
-			    break;
+            	dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
+                break;
 
-			case BlendModes.COLOR:
-				hsl  = rgb2YCbCr( r1, g1, b1 );
-				hsl2 = rgb2YCbCr( r2, g2, b2 );
+            case BlendModes.COLOR:
+            	hsl  = rgb2YCbCr( r1, g1, b1 );
+            	hsl2 = rgb2YCbCr( r2, g2, b2 );
                 rgb  = YCbCr2rgb( hsl.r, hsl2.g, hsl2.b );
 
                 dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
-			    break;
-
-			case BlendModes.HUE:
-				hsv  = rgb2hsv( r1, g1, b1 );
-				hsv2 = rgb2hsv( r2, g2, b2 );
-                rgb  = hsv2rgb( hsv2.h, hsv.s, hsv.v );
-
-				dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
-			    break;
-
-			case BlendModes.SATURATION:
-				hsv  = rgb2hsv( r1, g1, b1 );
-				hsv2 = rgb2hsv( r2, g2, b2 );
-                rgb  = hsv2rgb( hsv.h, hsv2.s, hsv.v );
-
-				dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
+            	dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
                 break;
 
-			case BlendModes.LIGHTER_COLOR:
-				if ( isLighter( r1, g1, b1, r2, g2, b2 )) {
+            case BlendModes.HUE:
+            	hsv  = rgb2hsv( r1, g1, b1 );
+            	hsv2 = rgb2hsv( r2, g2, b2 );
+                rgb  = hsv2rgb( hsv2.h, hsv.s, hsv.v );
+
+            	dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
+                break;
+
+            case BlendModes.SATURATION:
+            	hsv  = rgb2hsv( r1, g1, b1 );
+            	hsv2 = rgb2hsv( r2, g2, b2 );
+                rgb  = hsv2rgb( hsv.h, hsv2.s, hsv.v );
+
+            	dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
+                break;
+
+            case BlendModes.LIGHTER_COLOR:
+            	if ( isLighter( r1, g1, b1, r2, g2, b2 )) {
                     rgb = { r: r1, g: g1, b: b1 };
                 } else {
                     rgb = { r: r2, g: g2, b: b2 };
                 }
                 dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
+            	dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
                 break;
 
-			case BlendModes.DARKER_COLOR:
+            case BlendModes.DARKER_COLOR:
                 if ( !isLighter( r1, g1, b1, r2, g2, b2 )) {
                     rgb = { r: r1, g: g1, b: b1 };
                 } else {
                     rgb = { r: r2, g: g2, b: b2 };
                 }
-				dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
-				dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
-				dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
+            	dst[ px ]     = f1 * rgb.r + f2 * r1 + f3 * r2;
+            	dst[ px + 1 ] = f1 * rgb.g + f2 * g1 + f3 * g2;
+            	dst[ px + 2 ] = f1 * rgb.b + f2 * b1 + f3 * b2;
                 break;
         }
     }
