ed645330e59ff13af3d14cc70a40f0ac24f33b9f
Igor Zinken <730069+igorski@users.noreply.github.com>
2026-03-22 09:36:04 +0100

    Added initial utility to manage timelines

---
+++ tests/unit/utils/timeline-util.spec.ts
@@ -0,0 +1,61 @@
+import { it, describe, expect } from "vitest";
+import { mockZCanvas } from "../mocks";
+
+mockZCanvas();
+
+import DocumentFactory from "@/factories/document-factory";
+import LayerFactory from "@/factories/layer-factory";
+import {
+    getLayersByTile,
+    getIndexOfFirstLayerInTileGroup,
+    getIndexOfLastLayerInTileGroup,
+} from "@/utils/timeline-util";
+
+describe( "Timeline utilities", () => {
+    const tile1Layer1 = LayerFactory.create({ rel: { type: "tile", id: 0 }});
+    const tile1Layer2 = LayerFactory.create({ rel: { type: "tile", id: 0 }});
+
+    const tile2Layer1 = LayerFactory.create({ rel: { type: "tile", id: 1 }});
+    const tile2Layer2 = LayerFactory.create({ rel: { type: "tile", id: 1 }});
+    const tile2Layer3 = LayerFactory.create({ rel: { type: "tile", id: 1 }});
+
+    const tile3Layer1 = LayerFactory.create({ rel: { type: "tile", id: 2 }});
+
+    const activeDocument = DocumentFactory.create({
+        layers: [
+            tile1Layer1, tile1Layer2,
+            tile2Layer1, tile2Layer2, tile2Layer3,
+            tile3Layer1,
+        ]
+    });
+
+    it( "should be able to retrieve all Layers by their tile grouping", () => {
+        const tile1group = getLayersByTile( activeDocument, 0 );
+        const tile2group = getLayersByTile( activeDocument, 1 );
+        const tile3group = getLayersByTile( activeDocument, 2 );
+
+        expect( tile1group ).toEqual([ tile1Layer1, tile1Layer2 ]);
+        expect( tile2group ).toEqual([ tile2Layer1, tile2Layer2, tile2Layer3 ]);
+        expect( tile3group ).toEqual([ tile3Layer1 ]);
+    });
+
+    it( "should be able to get the indices of the first Layer within a tile group, relative to the Document layer list", () => {
+        const tile1firstIndex = getIndexOfFirstLayerInTileGroup( activeDocument, 0 );
+        const tile2firstIndex = getIndexOfFirstLayerInTileGroup( activeDocument, 1 );
+        const tile3firstIndex = getIndexOfFirstLayerInTileGroup( activeDocument, 2 );
+
+        expect( tile1firstIndex ).toEqual( 0 );
+        expect( tile2firstIndex ).toEqual( 2 );
+        expect( tile3firstIndex ).toEqual( 5 );
+    });
+
+    it( "should be able to get the indices of the last Layer within a tile group, relative to the Document layer list", () => {
+        const tile1lastIndex = getIndexOfLastLayerInTileGroup( activeDocument, 0 );
+        const tile2lastIndex = getIndexOfLastLayerInTileGroup( activeDocument, 1 );
+        const tile3lastIndex = getIndexOfLastLayerInTileGroup( activeDocument, 2 );
+
+        expect( tile1lastIndex ).toEqual( 1 );
+        expect( tile2lastIndex ).toEqual( 4 );
+        expect( tile3lastIndex ).toEqual( 5 );
+    });
+});
